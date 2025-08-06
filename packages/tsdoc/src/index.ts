be1454fe0207b3d67ed7b755bee79e4af558a141
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-08-06 12:22:12 +0300

    move nextra/tsdoc to `@nextra/tsdoc`

--- packages/tsdoc/src/index.ts
+++ packages/tsdoc/src/index.ts
@@ -1,1 +1,6 @@
-/* eslint-disable unicorn/no-empty-file */
+import 'server-only'
+
+export { TSDoc } from './tsdoc'
+export { generateDefinition } from './base'
+export { generateTsFromZod } from './zod-to-ts'
+export * from './types'
