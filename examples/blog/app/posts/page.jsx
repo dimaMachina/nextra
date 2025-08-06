a132a7569a87ccfd4fdfffe598dc007d41b716e9
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-08-06 14:44:37 +0300

    fix prettier

--- examples/blog/app/posts/page.jsx
+++ examples/blog/app/posts/page.jsx
@@ -1,5 +1,5 @@
-import Link from 'next/link'
 import { PostCard } from '@nextra/theme-blog'
+import Link from 'next/link'
 import { getPosts, getTags } from './get-posts'
 
 export const metadata = {
