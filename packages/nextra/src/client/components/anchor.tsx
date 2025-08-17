7d010e98fb7fca8befc2e5523895d2e382e93258
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-08-17 15:18:53 +0300

    fixes

--- packages/nextra/src/client/components/anchor.tsx
+++ packages/nextra/src/client/components/anchor.tsx
@@ -30,9 +30,9 @@ export const Anchor: FC<Props> = ({ href = '', prefetch, ...props }) => {
               <svg
                 fill="none"
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="1.7"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="1.7"
                 viewBox="0 0 24 24"
                 xmlns="http://www.w3.org/2000/svg"
                 // based on font-size
