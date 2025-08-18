03d45ec3499a0b775e33d4e202ca8bb8c7d1d072
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-08-18 12:12:29 +0300

    upd

--- packages/nextra/tsup.config.ts
+++ packages/nextra/tsup.config.ts
@@ -1,7 +1,12 @@
+import fs from 'node:fs/promises'
+import path from 'node:path'
+import { reactCompilerPlugin } from 'esbuild-react-compiler-plugin'
 import { defineConfig } from 'tsup'
 import { defaultEntry } from './default-entry.js'
 import packageJson from './package.json'
 import { IS_PRODUCTION } from './src/server/constants.js'
+
+const SEP = path.sep === '/' ? '/' : '\\\\'
 
 export default defineConfig({
   name: packageJson.name,
@@ -12,6 +17,18 @@ export default defineConfig({
   clean: IS_PRODUCTION,
   bundle: false,
   external: ['shiki', 'webpack'],
+  async onSuccess() {
+    // Fixes hydration errors in client apps due "type": "module" in root package.json
+    const clientPackageJSON = path.resolve('dist', 'client', 'package.json')
+    await fs.writeFile(clientPackageJSON, '{"sideEffects":false}')
+  },
+  esbuildPlugins: [
+    reactCompilerPlugin({
+      filter: new RegExp(
+        String.raw`/nextra/src/client/.+$`.replaceAll('/', SEP)
+      )
+    })
+  ],
   plugins: [
     {
       // Strip `node:` prefix from imports
